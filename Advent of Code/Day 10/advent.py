1e07a01bc8f5b3110772aa123294008cb05a100f
tkern0 <drivetkern@gmail.com>
2016-03-17 17:15:10 +1300

    Fixed AoC Day 10
    Wasn't resetting 'num' in between repetitions causing it to grow at at
    much higher rate, and eventually run out of memory.

--- Advent of Code/Day 10/advent.py
+++ Advent of Code/Day 10/advent.py
@@ -1,22 +1,16 @@
-num = "3113322113"
-combo = 1
-newNum = ""
-for repeat in range(40):
-    for i in range(len(num) - 1):
-        if num[i] == num[i + 1]:
-            combo += 1
-        else:
-            newNum += (str(combo) + num[i])
-            combo = 1
-    newNum += (str(combo) + num[-1])
+def look_and_say(num, rl, repeat = 1):
     combo = 1
-    num = newNum
-    print(str(int(100 * ((repeat + 1) / 40))) + "%")
-print("The length of the string is", len(num))
-# From goo.gl/dSJcrk:
-#from itertools import groupby
-#
-#def look_and_say(input_string, num_iterations):
-#    for i in xrange(num_iterations):
-#        input_string = ''.join([str(len(list(g))) + str(k) for k, g in groupby(input_string)])
-#    return input_string
+    for repeat in range(repeat):
+        newNum = ""
+        for i in range(len(num) - 1):
+            if num[i] == num[i + 1]: combo += 1
+            else:
+                newNum += (str(combo) + num[i])
+                combo = 1
+        newNum += (str(combo) + num[-1])
+        combo = 1
+        num = newNum
+    if rl: return len(num)
+    else: return num
+print("The length of the string in part 1 is", look_and_say("3113322113", True, 40))
+print("The length of the string in part 2 is", look_and_say("3113322113", True, 50))
