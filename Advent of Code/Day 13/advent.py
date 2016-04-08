4e176f25d9ff8ba34735c6984f47e2b9763f0487
tkern0 <drivetkern@gmail.com>
2016-04-08 19:17:01 +1200

    Made AoC Day 13 work

--- Advent of Code/Day 13/advent.py
+++ Advent of Code/Day 13/advent.py
@@ -1,10 +1,9 @@
 from itertools import permutations
-happiest = 0
 def format_input(txt):
     happy = {}
     people = []
     for line in txt:
-        words = line[:-2].split() # Removes fullstop and newline characters
+        words = line[:-2].split() # Removes fullstop and newline characters. Input needs blank line at the end because of this.
         words[3] = int(words[3]) if words[2] == "gain" else -int(words[3])
         happy[(words[0], words[10])] = words[3]
         # *Alice* would *lose* *81* happiness units by sitting next to *Carol*
@@ -13,13 +12,19 @@ def format_input(txt):
     return happy, people
 
 def find_happy(aPerson, bPerson):
-    if (aPerson, bPerson) in happy: return happy[(aPerson, bPerson)]
-    else: return happy[(bPerson, aPerson)]
+    if aPerson == "Me" or bPerson == "Me": return 0
+    else: return happy[(aPerson, bPerson)] + happy[(bPerson, aPerson)]
 
-happy, people = format_input(open("Day 13\input.txt"))
-for perm in permutations(people, len(people)):
-    currentHappy = find_happy(perm[0], perm[-1]) #TODO: Make it work
-    for i in range(len(perm) - 1):
-        currentHappy += find_happy(perm[i], perm[i + 1])
-    if currentHappy > happiest: happiest = currentHappy
-print("The happiest arangement creates", happiest, "happiness")
+def find_happiest():
+    happiest = 0
+    for perm in permutations(people):
+        currentHappy = find_happy(perm[0], perm[-1])
+        for i in range(len(perm) - 1):
+            currentHappy += find_happy(perm[i], perm[i + 1])
+        if currentHappy > happiest: happiest = currentHappy
+    return happiest
+
+happy, people = format_input(open("Day 13\\input.txt"))
+print("The happiest arangement creates", find_happiest(), "happiness")
+people.append("Me")
+print("The happiest arangement, including myself, creates", find_happiest(), "happiness")
