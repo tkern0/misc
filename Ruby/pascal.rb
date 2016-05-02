c36afce6035a4799683490371510d7c3475950af
tkern <drivetkern@gmail.com>
2016-05-02 13:44:49 +1200

    Another minor optimisation

--- Ruby/pascal.rb
+++ Ruby/pascal.rb
@@ -3,7 +3,7 @@ def pascal(n)
     for i in 1..n - 1
         x = [1]
         for j in 0..pa[-2].length
-            j == pa[-1].length - 1 ? (x.push(1)) : (x.push(pa[-1][j] + pa[-1][j + 1]))
+            j == pa[-2].length ? (x.push(1)) : (x.push(pa[-1][j] + pa[-1][j + 1]))
         end
         pa.push(x)
     end
