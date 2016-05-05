152ce4d97b9e0f9c658691a9dfbd85aa6ee48e60
tkern <drivetkern@gmail.com>
2016-05-05 13:57:41 +1200

    Ruby AoC Day 18 work

--- Advent of Code/Day 18/ruby.rb
+++ Advent of Code/Day 18/ruby.rb
@@ -1,17 +1,71 @@
-$size = 6
-$steps = 1
-$path = "Advent of Code/Day 18/test.txt"
+steps = 1
+file = File.open("Advent of Code/Day 18/test.txt").readlines
+$size = file.length
 
+# Turns input into hash array with empty border
 def format_input(startState)
-    state = Hash.new(nil)
-    for x in 0..$size + 1
-        for y in 0..$size + 1
-            state[[x, y]]
-        end
+    state = Hash.new(false)
+    for y in 0..$size - 1 # Because it goes line by line "y" comes first here
+    	for x in 0..$size - 1
+			state[[x + 1, y + 1]] = true if startState[y].split("")[x] == "#"
+    	end
     end
     return state
 end
 
-state = format_input(File.open($path))
-file = File.open($path).readlines
-p file
+# Wasn't working in "format_state()
+def get_cell(x, y, state)
+    return state[[x, y]]
+end
+
+# Formats hash array the same way as input
+def format_state(state)
+	formated = ""
+	for y in 1..$size # Builds line by line, so "y" comes first
+		for x in 1..$size
+			if get_cell(x, y, state)
+				formated << "#"
+			else
+				formated << "."
+			end
+		end
+		formated << "\n"
+	end
+	return formated
+end
+
+# Counts how many neighbours are "true"
+def get_neighbours(x, y, state)
+	count = 0
+	for ix in x - 1..x + 1
+		for iy in y - 1..y + 1
+			count += 1 if state[[ix, iy]] and not (ix == x and iy == y)
+		end
+	end
+	return count
+end
+
+# Advances "state" by one step each time it is called
+def advance(state)
+    newState = Hash.new(false)
+    for x in 1..$size
+        for y in 1..$size
+            neighbours = get_neighbours(x, y, state)
+            if state[[x, y]]
+                newState[[x, y]] = true if neighbours == 2 or neighbours == 3
+            else
+                newState[[x, y]] = true if neighbours == 3
+            end
+        end
+    end
+end
+
+state = format_input(file)
+
+print(format_state(state))
+
+for _ in 1..steps
+    state = advance(state)
+end
+
+print(format_state(state))
