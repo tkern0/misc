f2d37952d24ee0063ea6f838023d96b10bc07a1a
tkern <drivetkern@gmail.com>
2016-05-16 15:43:47 +1200

    AoC day 19 brainstorm

--- Advent of Code/Day 19/advent.py
+++ Advent of Code/Day 19/advent.py
@@ -1,13 +1,9 @@
-def join_molecule(split):
-    join = ""
-    for element in split: join += str(element)
-    return join
-
 # Turns the input into a bunch of useful vars
-replacements, recipe, splitMolecule, replaced = open("Advent of Code/Day 19/input.txt"), {}, [], []
+replacements, recipe, reverseRecipe, splitMolecule, replace = open("Advent of Code/Day 19/input.txt"), {}, {}, [], []
 for line in replacements: # Determines recipes and the molecule
     splitLine = line.strip().split(" ")
     if len(splitLine) == 3:
+        reverseRecipe[splitLine[2]] = splitLine[0]
         if splitLine[0] in recipe: recipe[splitLine[0]].append(splitLine[2])
         else: recipe[splitLine[0]] = [splitLine[2]]
     elif not line == "": molecule = line.strip()
@@ -20,5 +16,18 @@ for i in range(len(splitMolecule)): # Works out part 1
         for replace in recipe[splitMolecule[i]]:
             newMolecule = list(splitMolecule)
             newMolecule[i] = replace
-            if not join_molecule(newMolecule) in replaced: replaced.append(join_molecule(newMolecule))
+            join = ""
+            for element in newMolecule: join += str(element)
+            if not join in replaced: replaced.append(join)
 print("Part 1:", len(replaced))
+# recursive function:
+# does 1 reverse replacement everywhere possible
+# calls itself on new molecule
+# count steps somehow
+#
+# get min + max len of outputs
+# start at [0:], check if following chars, from min --> max len, are possible recipe output
+# if so, replace with input, go on to next one
+# then start at [1:]
+# repeat
+for i in range(len(molecule)):
