2d86c35916ad943bf63075b57e54b63be67fc5d7
tkern <drivetkern@gmail.com>
2016-05-26 13:54:49 +1200

    formatting

--- Advent of Code/Day 20/advent.py
+++ Advent of Code/Day 20/advent.py
@@ -1,4 +1,4 @@
 findNum, i = 33100000/10, 0
-def find_presents(num): return sum([i + 1 if num % (i + 1) == 0 else 0 for i in range(num )])
+def find_presents(n): return sum([i + 1 if n % (i + 1) == 0 else 0 for i in range(n)])
 while find_presents(i) < findNum: i += 1
 print(i)
