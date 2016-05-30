44c35d0beb9af5e13dc420b78977d665c3f79e6f
tkern <drivetkern@gmail.com>
2016-05-30 13:54:44 +1200

    look + say works

--- Ruby/lookandsay.rb
+++ Ruby/lookandsay.rb
@@ -1,7 +1,7 @@
 def look_and_say(n)
     combo = 1
     newNum = ""
-    for i in 0..n - 1 do
+    for i in 0..n.length - 1 do
         if n[i] == n[i + 1] then
             combo += 1
         else
@@ -9,11 +9,9 @@ def look_and_say(n)
             combo = 1
         end
     end
-    newNum += combo.to_s + n[i - 1].to_s
-    combo = 1
     return newNum
 end
 
-p look_and_say(12)
+p look_and_say("3112")
 # "202190"
 # what
