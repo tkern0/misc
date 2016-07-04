1e561842bc331990d2fdb77409088fd3aae707b4
tkern <drivetkern@gmail.com>
2016-07-04 13:45:52 +1200

    1-line prime

--- Python/primes.py
+++ Python/primes.py
@@ -2,5 +2,9 @@ def is_prime(n):
     for i in range(2, int(n**0.5) + 1):
         if n % i == 0: return False
     return n > 1
+
+# More inefficent, but 1-liner
+def prime(n): return True not in [n % i == 0 for i in range(2, int(n**0.5) + 1)] and n > 1
+
 for i in range(100):
     if is_prime(i): print(i)
