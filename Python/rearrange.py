350c7a6cae96e632c2ae980bd8b30fda07ecd9c8
tkern0 <tkern9182@gmail.com>
2017-09-09 10:23:20 +1200

    added rearrange.py

---
+++ Python/rearrange.py
@@ -0,0 +1,24 @@
+import enchant, itertools
+a = enchant.Dict("en_US")
+b = enchant.Dict("en_GB")
+# for i in itertools.permutations("ANGEL"):
+#     if a.check("".join(i)) or b.check("".join(i)):
+#         print("".join(i))
+
+f = set()
+for i in itertools.permutations((["A", "V"], ["N", "Z"], ["G"], ["L", "T"], ["E", "M", "W"])):
+    for j in itertools.product(range(3), repeat=5):
+        s = ""
+        for k in range(5):
+            s += i[k][j[k]%len(i[k])]
+        if (a.check(s) or b.check(s)) and s not in f:
+            print(s)
+            f.add(s)
+# ANGLE
+# ANGEL
+# AGENT
+# GALEN
+# GLAZE
+# GLEAN
+# LANGE
+# TWANG
